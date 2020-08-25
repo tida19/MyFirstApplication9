b9c005022fbef4f101b2216470572f5657cba63c
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2020-08-25 16:41:19 +0700

    ทำ ConDatabase

--- app/src/main/java/com/example/myfirstapplication9/data/ConDatabase.kt
+++ app/src/main/java/com/example/myfirstapplication9/data/ConDatabase.kt
@@ -1,4 +1,33 @@
 package com.example.myfirstapplication9.data
 
-class ConDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ContactData::class],version = 1)
+abstract class ConDatabase : RoomDatabase(){
+
+    abstract val conDatabaseDao: ConDatabaseDao
+    companion object{
+        private var INSTANCE: ConDatabase? = null
+
+        fun  getInstance(context: Context): ConDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ConDatabase::class.java,
+                        "contact_his_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
