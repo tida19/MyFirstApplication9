da68fc22b13ab4a82196fd84ec1dc8af49be8d3c
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2020-08-25 16:20:17 +0700

    set fragment_Contact/add data to the layout/ recyclerview / button

--- app/src/main/java/com/example/myfirstapplication9/contact/Contactfragment.kt
+++ app/src/main/java/com/example/myfirstapplication9/contact/Contactfragment.kt
@@ -8,14 +8,14 @@ import android.view.ViewGroup
 import com.example.myfirstapplication9.R
 
 
-class Contactragment : Fragment() {
+class Contactfragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contactragment, container, false)
+        return inflater.inflate(R.layout.fragment_contactfragment, container, false)
     }
 
 
