3c767053dce9fafa1db39df79d2d99dcef49c66d
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2020-08-25 16:44:00 +0700

    ทำ ConDatabaseDao

--- app/src/main/java/com/example/myfirstapplication9/data/ConDatabaseDao.kt
+++ app/src/main/java/com/example/myfirstapplication9/data/ConDatabaseDao.kt
@@ -1,4 +1,15 @@
 package com.example.myfirstapplication9.data
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface ConDatabaseDao {
+    @Insert
+    fun insert(contact: ContactData)
+
+    @Query("select * from table_contact order by contactId DESC")
+    fun getName(): LiveData<List<ContactData>>
 }
